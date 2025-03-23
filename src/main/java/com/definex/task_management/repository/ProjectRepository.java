7a0561cf7984b6321e3b7b6e7f91b466175667d1
Umut Tölek <umuttolekk@gmail.com>
2025-03-23 14:58:18 +0300

    Intermediate Service Deleted
    Created domain sql queries to enhance and get rid of redundant code

--- src/main/java/com/definex/task_management/repository/ProjectRepository.java
+++ src/main/java/com/definex/task_management/repository/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
@@ -13,4 +14,7 @@ public interface ProjectRepository extends JpaRepository<Project, UUID> {
 
     @Query("SELECT p FROM Project p JOIN p.teamMembers m WHERE p.department = :department AND m.id = :userId")
     List<Project> findByDepartmentAndTeamMembersContaining(@Param("department") String department, @Param("userId") UUID userId);
+    
+    @Query(value = "SELECT p.* FROM projects p LEFT JOIN project_team_members ptm ON p.id = ptm.project_id WHERE p.id = :projectId AND ptm.user_id = :userId", nativeQuery = true)
+    Optional<Project> findByIdAndUserAccess(@Param("projectId") UUID projectId, @Param("userId") UUID userId);
 }
